b9c0921c8e581c888502235b0692acc2a53b911c
fviscomi <f.viscomi@redev.technology>
2021-09-12 17:55:57 +0100

    edn of lecture 45

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/config/WebSecurityConfig.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/config/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser(userConfigData.getUserName())
-                .password(userConfigData.getPassword())
+                .password(passwordEncoder().encode(userConfigData.getPassword()))
                 .roles(userConfigData.getRoles());
     }
 
