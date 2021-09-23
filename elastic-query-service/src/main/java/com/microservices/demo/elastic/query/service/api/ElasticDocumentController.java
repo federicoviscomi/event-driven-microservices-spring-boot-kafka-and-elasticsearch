58c54172e75dc015d4b1423033739bb20c72e69e
fviscomi <f.viscomi@redev.technology>
2021-09-23 18:17:57 +0100

    end of lecture 60

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,6 +28,9 @@ public class ElasticDocumentController {
     public ElasticDocumentController(ElasticQueryService queryService) {
         this.elasticQueryService = queryService;
     }
+
+    @Value("${server.port}")
+    private String port;
 
     @Operation(summary = "Get all elastic documents.")
     @ApiResponses(value = {
@@ -46,6 +50,7 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(response);
     }
 
+
     @Operation(summary = "Get elastic document by id.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful response.", content = {
@@ -61,7 +66,7 @@ public class ElasticDocumentController {
     ResponseEntity<ElasticQueryServiceResponseModel>
     getDocumentById(@PathVariable @NotEmpty String id) {
         ElasticQueryServiceResponseModel elasticQueryServiceResponseModel = elasticQueryService.getDocumentById(id);
-        LOG.debug("Elasticsearch returned document with id {}", id);
+        LOG.debug("Elasticsearch returned document with id {} on port {}", id, port);
         return ResponseEntity.ok(elasticQueryServiceResponseModel);
     }
 
@@ -81,14 +86,8 @@ public class ElasticDocumentController {
     getDocumentByText(@RequestBody @Valid ElasticQueryServiceRequestModel elasticQueryServiceRequestModel) {
         List<ElasticQueryServiceResponseModel> response =
                 elasticQueryService.getDocumentByText(elasticQueryServiceRequestModel.getText());
-        LOG.info("Elasticsearch returned {} of documents", response.size());
+        LOG.info("Elasticsearch returned {} of documents on port {}", response.size(), port);
         return ResponseEntity.ok(response);
     }
 
-    /*
-    @PostMapping("/delete-all-documents")
-    public @ResponseBody ? deleteAllDocuments() {
-        elasticQueryService.deleteAllDocuments();
-    }
-    */
 }
