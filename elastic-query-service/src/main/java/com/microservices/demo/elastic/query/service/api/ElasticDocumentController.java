0ec0b9873697810bd5e3ffbb70600eb1bcc00afd
fviscomi <f.viscomi@redev.technology>
2021-10-07 11:36:45 +0100

    end of section 8

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -1,8 +1,8 @@
 package com.microservices.demo.elastic.query.service.api;
 
 import com.microservices.demo.elastic.query.service.business.ElasticQueryService;
-import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceRequestModel;
-import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import com.microservices.demo.elastic.query.service.common.model.ElasticQueryServiceRequestModel;
+import com.microservices.demo.elastic.query.service.common.model.ElasticQueryServiceResponseModel;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
---
+++ reactive-elastic-query-service/src/main/java/com/microservices/demo/reactive/elastic/query/service/ReactiveElasticQueryServiceApplication.java
@@ -0,0 +1,9 @@
+package com.microservices.demo.reactive.elastic.query.service;
+
+import org.springframework.boot.SpringApplication;
+
+public class ReactiveElasticQueryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveElasticQueryServiceApplication.class, args);
+    }
+}
